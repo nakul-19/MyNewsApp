9546219900ac541d0822fadef7665b635e69d8b2
Nakul Sharma <rnqnkl@gmail.com>
2020-09-21 16:32:23 +0530

    Added api calls to fetch latest data.

--- app/src/main/java/com/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/mynewsapp/MainActivity.kt
@@ -1,23 +1,98 @@
 package com.mynewsapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+const val BASE_URL = "https://api.currentsapi.services"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var countDownTimer: CountDownTimer
+
+    private var headingList= mutableListOf<String>()
+    private var contentList=mutableListOf<String>()
+    private var imageList=mutableListOf<String>()
+    private var linksList=mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val news= listOf<String>("X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already",
-            "X died","Y died","2020 sucks","enough of this year already")
-        val newsList = findViewById<RecyclerView>(R.id.my_recycler_view)
-        newsList.layoutManager = LinearLayoutManager(this)
-        newsList.adapter = NewsListAdapter(news)
+        makeRequest()
+    }
+
+    private fun setScreen(){
+        recyclerView.setBackgroundColor(Color.rgb(255,255,255))
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = NewsListAdapter(headingList,contentList,imageList,linksList)
+    }
+
+    private fun addNews(heading:String,content:String,image:String,link:String){
+        headingList.add(heading)
+        contentList.add(content)
+        imageList.add(image)
+        linksList.add(link)
+    }
+
+    private fun fadeIn(){
+        initialScreen.animate().apply {
+            alpha(0f)
+            duration=3000
+        }.start()
+    }
+
+    private fun makeRequest() {
+
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(APIrequest::class.java)
+
+        progressBar.visibility=View.VISIBLE
+
+        GlobalScope.launch(Dispatchers.IO){
+
+            try {
+                val response = api.getNews()
+                for (i in response.news)
+                    addNews(i.title,i.description,i.image,i.url)
+
+                withContext(Dispatchers.Main){
+                    setScreen()
+                    fadeIn()
+                    progressBar.visibility=View.GONE
+                }
+            }
+            catch (e : Exception){
+
+                withContext(Dispatchers.Main){
+
+                    countDownTimer= object : CountDownTimer(5000,1000) {
+                        override fun onTick(millisUntilFinished: Long) {
+                        }
+
+                        override fun onFinish() {
+                            makeRequest()
+                            countDownTimer.cancel()
+                        }
+                    }
+                    countDownTimer.start()
+                }
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/mynewsapp/NewsListAdapter.kt
+++ app/src/main/java/com/mynewsapp/NewsListAdapter.kt
@@ -1,22 +1,51 @@
 package com.mynewsapp
 
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class NewsListAdapter(val items: List<String>) :
+class NewsListAdapter(var heading: List<String>,
+                      var content: List<String>,
+                      var images: List<String>,
+                      var link: List<String>) :
     RecyclerView.Adapter<NewsListAdapter.ViewHolder>() {
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    inner class ViewHolder(val newsView: View) : RecyclerView.ViewHolder(newsView){
+        val newsHeading = newsView.findViewById<TextView>(R.id.heading)
+        val newsContent = newsView.findViewById<TextView>(R.id.content)
+        val newsImage = newsView.findViewById<ImageView>(R.id.image)
+
+        init {
+            newsView.setOnClickListener {v: View? ->
+                val position = adapterPosition
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(link[position])
+                startActivity(newsView.context, intent, null)
+            }
+        }
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val v : View = LayoutInflater.from(parent.context).inflate(R.layout.news_layout,parent,false)
+        return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text=items[position]
+        holder.newsHeading.text=heading[position]
+        holder.newsContent.text=content[position]
+        Glide.with(holder.newsImage)
+            .load(images[position])
+            .into(holder.newsImage)
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = heading.size
 }
